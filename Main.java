4f8ec00adc42858215ddfce0cf210caef7c5c3fe
rain chen <rainc8@nycstudents.net>
2024-11-18 12:30:04 -0500

    updated main

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args){
-
+        boolean [][] schedule = new boolean[8][60];
+        for (int i = 10; i < 15; i++) {schedule[1][i] = true;}
+        AppointmentBook a = new AppointmentBook(schedule);
+        a.printPeriod(2);
+        a.findFreeBlock(2,15);
     }
 }
